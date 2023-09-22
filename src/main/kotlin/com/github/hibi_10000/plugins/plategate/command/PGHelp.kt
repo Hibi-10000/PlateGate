30c56f096d04d2ccba43f709b8ae24cd9f622882
Hibi_10000 <87810571+Hibi-10000@users.noreply.github.com>
2023-09-23 02:25:42 +0900

    helpコマンドのリファクタリング

--- src/main/kotlin/com/github/hibi_10000/plugins/plategate/command/PGHelp.kt
+++ src/main/kotlin/com/github/hibi_10000/plugins/plategate/command/PGHelp.kt
@@ -17,7 +17,6 @@ class PGHelp {
     @Suppress("UNUSED_PARAMETER")
     fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<String>): Boolean {
         if (!util.checkPermission(sender, "plategate.command.help")) return false
-        if (!(args.size == 2 || args.size == 1)) return util.commandInvalid(sender, label)
         if (args.size == 2) {
             if (args[1].equals("plugin", ignoreCase = true)) {
                 sender.sendMessage("§a[PlateGate $version] §6MoreHelp")
@@ -28,99 +27,53 @@ class PGHelp {
                 sender.sendMessage("License: Mozilla Public License 2.0")
                 return true
             }
-            val help =
-                TextComponent("§a[PlateGate] §cコマンドが間違っています。 /$label help で使用法を確認してください。")
-            help.hoverEvent = HoverEvent(
-                HoverEvent.Action.SHOW_TEXT,
-                Text("§aクリックで§b\"/$label help\"§aを実行")
-            )
-            help.clickEvent = ClickEvent(ClickEvent.Action.RUN_COMMAND, "/$label help")
-            sender.spigot().sendMessage(help)
-            return false
         }
+        if (args.size != 1) return util.commandInvalid(sender, label)
         sender.sendMessage("§a[PlateGate $version] §6Help")
         sender.sendMessage(" §6Command§r:")
-        val help1 = TextComponent(" - §b/$label create [GateName]")
-        help1.hoverEvent = HoverEvent(
-            HoverEvent.Action.SHOW_TEXT,
-            Text("§aクリックで§b\"/$label create\"§aをチャットにセット")
-        )
-        help1.clickEvent = ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/$label create ")
-        sender.spigot().sendMessage(help1)
-        val help2 = TextComponent(" - §b/$label move [GateName]")
-        help2.hoverEvent = HoverEvent(
-            HoverEvent.Action.SHOW_TEXT,
-            Text("§aクリックで§b\"/$label move\"§aをチャットにセット")
-        )
-        help2.clickEvent = ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/$label move ")
-        sender.spigot().sendMessage(help2)
-        val help3 = TextComponent(" - §b/$label delete [GateName]")
-        help3.hoverEvent = HoverEvent(
-            HoverEvent.Action.SHOW_TEXT,
-            Text("§aクリックで§b\"/$label delete\"§aをチャットにセット")
-        )
-        help3.clickEvent = ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/$label delete ")
-        sender.spigot().sendMessage(help3)
-        val help4 = TextComponent(" - §b/$label link [GateName:ここから] [GateName:ここへ飛ぶ]")
-        help4.hoverEvent = HoverEvent(
-            HoverEvent.Action.SHOW_TEXT,
-            Text("§aクリックで§b\"/$label link\"§aをチャットにセット")
-        )
-        help4.clickEvent = ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/$label link ")
-        sender.spigot().sendMessage(help4)
+
+        sender.spigot().sendMessage(commandHelp(label, "create", "[GateName]"))
+        sender.spigot().sendMessage(commandHelp(label,   "move", "[GateName]"))
+        sender.spigot().sendMessage(commandHelp(label, "delete", "[GateName]"))
+        sender.spigot().sendMessage(commandHelp(label,   "link", "[GateName:ここから] [GateName:ここへ飛ぶ]"))
+        sender.spigot().sendMessage(commandHelp(label, "modify", "[TargetGateName] [Object:<name/owner(OPOnly)>] [値:<NewGateName/Player>]"))
+        sender.spigot().sendMessage(commandHelp(label,   "list", "[Player(OPOnly)]"))
+        sender.spigot().sendMessage(commandHelp(label,   "jump", "[GateName]"))
+
+        val help = TextComponent(" - §b/$label help")
+        help.hoverEvent = HoverEvent(HoverEvent.Action.SHOW_TEXT, Text("§aクリックで§b\"/$label help\"§aを実行"))
+        help.clickEvent = ClickEvent(ClickEvent.Action.RUN_COMMAND, "/$label help")
+        sender.spigot().sendMessage(help)
 
         /*
-		TextComponent help5 = new TextComponent(" - §b/" + label +" modify [TargetGateName] [Object:<name/owner(OPOnly)>] [値:<NewGateName/Player>]");
-		help5.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("§aクリックで§b\"/" + label + " modify\"§aをチャットにセット")));
-		help5.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/" + label + " modify "));
-		sender.spigot().sendMessage(help5);
-		*/
-        val help6 = TextComponent(" - §b/$label list [Player(OPOnly)]")
-        help6.hoverEvent = HoverEvent(
-            HoverEvent.Action.SHOW_TEXT,
-            Text("§aクリックで§b\"/$label list\"§aをチャットにセット")
-        )
-        help6.clickEvent = ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/$label list ")
-        sender.spigot().sendMessage(help6)
-        val help7 = TextComponent(" - §b/$label help")
-        help7.hoverEvent = HoverEvent(
-            HoverEvent.Action.SHOW_TEXT,
-            Text("§aクリックで§b\"/$label help\"§aを実行")
-        )
-        help7.clickEvent = ClickEvent(ClickEvent.Action.RUN_COMMAND, "/$label help")
-        sender.spigot().sendMessage(help7)
-        val help8 = TextComponent(" - §b/$label jump [GateName]")
-        help8.hoverEvent = HoverEvent(
-            HoverEvent.Action.SHOW_TEXT,
-            Text("§aクリックで§b\"/$label jump\"§aをチャットにセット")
-        )
-        help8.clickEvent = ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/$label jump ")
-        sender.spigot().sendMessage(help8)
-
-        /*
-		sender.sendMessage(" §6Permission:§r");
-		
-		sender.sendMessage(" - §bplategate.");
-		sender.sendMessage(" - §bplategate.");
-		sender.sendMessage(" - §bplategate.");
-		sender.sendMessage(" - §bplategate.");
-		sender.sendMessage(" - §bplategate.");
-		sender.sendMessage(" - §bplategate.");
-		sender.sendMessage(" - §bplategate.");
-		*/
         sender.sendMessage("")
-        val morehelp = TextComponent(" §aHelpPlugin")
-        morehelp.hoverEvent = HoverEvent(
-            HoverEvent.Action.SHOW_TEXT,
-            Text("§aクリックで§b\"/$label help plugin\"§aを実行")
-        )
-        morehelp.clickEvent = ClickEvent(ClickEvent.Action.RUN_COMMAND, "/$label help plugin")
-        sender.spigot().sendMessage(morehelp)
+		sender.sendMessage(" §6Permission:§r");
+		sender.sendMessage(" - §bplategate.");
+		sender.sendMessage(" - §bplategate.");
+		sender.sendMessage(" - §bplategate.");
+		sender.sendMessage(" - §bplategate.");
+		sender.sendMessage(" - §bplategate.");
+		sender.sendMessage(" - §bplategate.");
+		sender.sendMessage(" - §bplategate.");
+		*/
+
+        sender.sendMessage("")
+        val moreHelp = TextComponent(" §aHelpPlugin")
+        moreHelp.hoverEvent = HoverEvent(HoverEvent.Action.SHOW_TEXT, Text("§aクリックで§b\"/$label help plugin\"§aを実行"))
+        moreHelp.clickEvent = ClickEvent(ClickEvent.Action.RUN_COMMAND, "/$label help plugin")
+        sender.spigot().sendMessage(moreHelp)
         return true
     }
 
     @Suppress("UNUSED_PARAMETER")
     fun onTabComplete(sender: CommandSender, command: Command, alias: String, args: Array<String>): List<String>? {
         return null
+    }
+
+    private fun commandHelp(label: String, command: String, options: String): TextComponent {
+        val help = TextComponent(" - §b/$label $command $options")
+        help.hoverEvent = HoverEvent(HoverEvent.Action.SHOW_TEXT, Text("§aクリックで§b\"/$label $command\"§aをチャットにセット"))
+        help.clickEvent = ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/$label $command")
+        return help
     }
 }
