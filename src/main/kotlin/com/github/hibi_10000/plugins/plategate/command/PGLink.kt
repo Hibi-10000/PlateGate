f3ba12b3257bc5eb70649b286ea6008fcc1da0c0
Hibi_10000 <87810571+Hibi-10000@users.noreply.github.com>
2023-09-28 23:08:30 +0900

    linkコマンドのリファクタリング

--- src/main/kotlin/com/github/hibi_10000/plugins/plategate/command/PGLink.kt
+++ src/main/kotlin/com/github/hibi_10000/plugins/plategate/command/PGLink.kt
@@ -16,11 +16,10 @@ class PGLink {
         if (!util.checkPermission(sender, "plategate.command.link")) return false
         if (args.size != 3) return util.commandInvalid(sender, label)
 
-        //new JsonHandler(plugin).JsonChange(args[1], null, null, args[2], null, null, null);
         val index = dbUtil.firstIndexJson("name", args[1], sender as Player) ?: return false
         dbUtil.setJson(index, "to", args[2], sender)
-        sender.sendMessage("§a[PlateGate] §bゲート " + args[1] + " から ゲート " + args[2] + " の方向にゲートをリンクしました。")
-        println("§a[PlateGate] §bゲート " + args[1] + " から ゲート " + args[2] + " の方向にゲートをリンクしました。")
+        sender.sendMessage("§a[PlateGate] §bゲート ${args[1]} から ゲート ${args[2]} の方向にゲートをリンクしました。")
+        println("§a[PlateGate] §bゲート ${args[1]} から ゲート ${args[2]} の方向にゲートをリンクしました。")
         return true
     }
 
